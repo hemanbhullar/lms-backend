0a59900f7d29b2aa3f01daae802929c7d4b88fd5
hemanbhullar <hemanbhullar8082@gmail.com>
2024-01-15 14:39:11 +0530

    Lms backend

---
+++ routes/course.routes.js
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+import { addLectureToCourseById, createCourse, getAllCourses, getLecturesByCourseId, removeCourse, updateCourse } from '../controllers/course.controller.js';
+import { authorizeSubscriber, authorizedRoles, isLoggedIn } from '../middlewares/auth.middleware.js';
+import upload from "../middlewares/multer.middleware.js";
+
+const router = Router();
+
+// router.get('/', getAllCourses);
+router.route('/')
+.get(getAllCourses)
+.post(
+    isLoggedIn,
+    authorizedRoles('ADMIN'),
+    upload.single('thumbnail'),
+    createCourse
+);
+//post etc you can create different types of requests from the same routes;
+
+// router.get('/:id', getLecturesByCourseId);
+
+router.route('/:id')
+.get(isLoggedIn, authorizeSubscriber, getLecturesByCourseId)
+.put(
+    isLoggedIn,
+    authorizedRoles('ADMIN'),
+    updateCourse
+    )
+.delete(
+    isLoggedIn,
+    authorizedRoles('ADMIN'),
+    removeCourse
+    )
+.post(
+    isLoggedIn,
+    authorizedRoles('ADMIN'),
+    upload.single('lecture'),
+    addLectureToCourseById
+);
+
+export default router;
